dacddfdae1ad98ca85d7c6d3d3fcb974b87339b0
Irene PR <72157834+ipice1618@users.noreply.github.com>
2022-07-09 18:30:48 +0200

    completed stringCalculator kata

--- src/fun.ts
+++ src/fun.ts
@@ -1,7 +1,77 @@
-export default function fun(string: string) {
-  return string;
+const isNumber = (num: number): boolean =>
+  typeof num === "number" && !Number.isNaN(num);
+
+const isNegative = (num: number): boolean => num < 0;
+
+const isLesserThan1000 = (num: number) => num < 1001;
+
+const splitAndFlatten = (strings: string[], separator: string): string[] =>
+  strings.map((string) => string.split(separator)).flat();
+
+const getInner = (
+  str: string,
+  opener: string,
+  closer: string
+): [string, string] | 1 | -1 => {
+  const nextIndex = str.indexOf(opener, 0);
+  if (nextIndex === -1) return 1;
+
+  const closingIndex = str.indexOf(closer, nextIndex);
+  if (closingIndex === -1) return -1;
+  const innerString = str.slice(nextIndex + 2, closingIndex);
+  const remainingString = str.slice(closingIndex + closer.length);
+
+  return [innerString, remainingString];
+};
+
+const findCustomSeparator = (
+  string: string,
+  array: string[] = []
+): [string, string[]] => {
+  const result = getInner(string, "//", "\n");
+  if (result === -1) {
+    throw Error("Invalid string of characters");
+  }
+  if (result === 1) return [string, array];
+  // eslint-disable-next-line prefer-const
+  let [possibleSeparator, remainingString] = result;
+  string = remainingString;
+
+  const cleanedUpSeparators = splitAndFlatten(
+    splitAndFlatten([possibleSeparator], "["),
+    "]"
+  ).filter((separator) => separator !== "");
+  array.push(...cleanedUpSeparators);
+
+  return [string, array];
+};
+
+export default function fun(string: string): number {
+  if (!string) return 0;
+
+  const stringAsNumber = +string;
+  if (isNumber(stringAsNumber)) return stringAsNumber;
+
+  const separators = [",", "\n"];
+  const [newString, newSeparators] = findCustomSeparator(string);
+  string = newString;
+  separators.push(...newSeparators);
+
+  let splitStrings = [string];
+  for (const separator of separators) {
+    splitStrings = splitAndFlatten(splitStrings, separator);
+  }
+
+  const splitNumbers = splitStrings.map((splitString) => +splitString);
+  if (!splitNumbers.every(isNumber))
+    throw new Error("Invalid string of characters");
+  if (splitNumbers.some(isNegative)) {
+    const negatives = splitNumbers.filter(isNegative);
+    const formattedNegatives = negatives.join(", ");
+    throw new Error(`error: negatives not allowed: ${formattedNegatives}`);
+  }
+
+  return splitNumbers
+    .filter(isLesserThan1000)
+    .reduce((previous, current) => previous + current, 0);
 }
-
-const hola = "hola";
-
-fun(hola);
--- tests/fun.test.ts
+++ tests/fun.test.ts
@@ -1,11 +1,130 @@
 import fun from "../src/fun";
 
 describe("Given a fun function", () => {
-  it("should return a string passed by parameters", () => {
-    const string = "patata";
+  describe("When passing a string not convertible to numbers", () => {
+    it("should throw an error", () => {
+      const string = "a";
+      const expectedError = "Invalid string of characters";
 
-    const result = fun(string);
+      const result = () => fun(string);
 
-    expect(result).toBe(string);
+      expect(result).toThrowError(expectedError);
+    });
+  });
+  describe("When passing an empty string", () => {
+    it("should return a 0", () => {
+      const string = "";
+      const expectedResult = 0;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a number typed as a string", () => {
+    it("should return the number, typed as a number", () => {
+      const string = "4";
+      const expectedResult = 4;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a string of two numbers separated by commas", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "4,3";
+      const expectedResult = 7;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a string of numbers separated by commas", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "4,3,5,1,7,6";
+      const expectedResult = 26;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a string of numbers separated by commas and '\n'", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "4,3,5\n1,7,6\n10";
+      const expectedResult = 36;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a string of numbers it will ignore numbers greater than 1000", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "2, 1003, 4";
+      const expectedResult = 6;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a string of numbers alongside a custom separator\n'", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "//;\n1;2";
+      const expectedResult = 3;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When passing a string of numbers alongside a // withou it being a custom separator\n'", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "//1,2";
+      const expectedError = "Invalid string of characters";
+
+      const result = () => fun(string);
+
+      expect(result).toThrowError(expectedError);
+    });
+  });
+  describe("When passing a string of numbers with negative numbers\n'", () => {
+    it("throw an error naming the negatives", () => {
+      const string = "1,-2,-3";
+      const expectedError = "error: negatives not allowed: -2, -3";
+
+      const result = () => fun(string);
+
+      expect(result).toThrowError(expectedError);
+    });
+  });
+  describe("When passing a string of numbers alongside a custom separator\n'", () => {
+    it("should return the sum of the numbers", () => {
+      const string = "//[***]\n1***2***3";
+      const expectedResult = 6;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+    it("should return the sum of the numbers", () => {
+      const string = "//[foo][bar]\n1foo2bar3";
+      const expectedResult = 6;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
+    it("should return the sum of the numbers", () => {
+      const string = "//[*][%]\n1*2%3";
+      const expectedResult = 6;
+
+      const result = fun(string);
+
+      expect(result).toBe(expectedResult);
+    });
   });
 });

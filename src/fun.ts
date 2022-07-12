50112453398b45d9610288cd5743958d9f135a5e
Irene PR <72157834+ipice1618@users.noreply.github.com>
2022-07-12 22:43:41 +0200

    kata rover not finished

--- src/fun.ts
+++ src/fun.ts
@@ -1,7 +1,53 @@
-export default function fun(string: string) {
-  return string;
+export default class Rover {
+  X: number = 0;
+
+  Y: number = 0;
+
+  direction: string = "N";
+
+  execute(moveCommand: string) {
+    const moveCommandArray = moveCommand.split("");
+
+    moveCommandArray.forEach((command) => {
+      if (command === "M") {
+        this.move();
+      }
+      if (command === "L") {
+        this.rotateLeft();
+      }
+      if (command === "R") {
+        this.rotateRight();
+      }
+    });
+
+    return this.printPosition();
+  }
+
+  private move() {
+    this.Y = (this.Y + 1) % 10;
+  }
+
+  private rotateLeft() {
+    if (this.direction === "N") {
+      this.direction = "W";
+    } else if (this.direction === "W") {
+      this.direction = "S";
+    } else if (this.direction === "S") {
+      this.direction = "E";
+    } else if (this.direction === "E") {
+      this.direction = "N";
+    }
+  }
+
+  private rotateRight() {
+    if (this.direction === "N") {
+      this.direction = "E";
+    } else if (this.direction === "E") {
+      this.direction = "S";
+    }
+  }
+
+  private printPosition() {
+    return `${this.X}:${this.Y}:${this.direction}`;
+  }
 }
-
-const hola = "hola";
-
-fun(hola);
--- tests/fun.test.ts
+++ tests/fun.test.ts
@@ -1,11 +1,75 @@
-import fun from "../src/fun";
+import Rover from "../src/fun";
 
-describe("Given a fun function", () => {
-  it("should return a string passed by parameters", () => {
-    const string = "patata";
+describe("Given a rover class", () => {
+  it("should reproduce instance of Rover", () => {
+    const roverInstance = new Rover();
 
-    const result = fun(string);
+    expect(roverInstance).toBeInstanceOf(Rover);
+  });
+  it("should execute given no input", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:N";
 
-    expect(result).toBe(string);
+    expect(roverInstance.execute("")).toBe(expectedResult);
+  });
+  it("facing north when left command is provided, should rotate direction to west", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:W";
+
+    expect(roverInstance.execute("L")).toBe(expectedResult);
+  });
+  it("facing north when right command is provided, should rotate direction to east", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:E";
+
+    expect(roverInstance.execute("R")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should advance one position facing north", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:1:N";
+
+    expect(roverInstance.execute("M")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should advance three positions facing north", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:3:N";
+
+    expect(roverInstance.execute("MMM")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should advance wrap around the grid when going over the limit facing north", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:N";
+
+    expect(roverInstance.execute("MMMMMMMMMM")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should advance wrap around the grid when going over the limit facing north", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:N";
+
+    expect(roverInstance.execute("MMMMMMMMMM")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should rotate towards the left two times and land facing south ", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:S";
+
+    expect(roverInstance.execute("LL")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should rotate towards the right two times and land facing south ", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:S";
+
+    expect(roverInstance.execute("RR")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should rotate towards the left and give a complete rotation ", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:E";
+
+    expect(roverInstance.execute("LLL")).toBe(expectedResult);
+  });
+  it("starting from the initial position, it should rotate towards the left and give a complete rotation ", () => {
+    const roverInstance = new Rover();
+    const expectedResult = "0:0:N";
+
+    expect(roverInstance.execute("LLLL")).toBe(expectedResult);
   });
 });
